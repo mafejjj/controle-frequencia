f88f302ac54d1a0fb758ec7756d10caccb1eb144
mafejjj <mariafernandadejesus28@gmail.com>
2026-09-03 11:31:45 -0300

    Configura projeto Spring Boot e PostgreSQL

---
+++ src/test/java/com/controlefrequencia/ControleFrequenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.controlefrequencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleFrequenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
